ca7e33813ae9ff7ee04e0d1c4dc4343a8c8dd00c
vbhasker <vinodh.ab@gmail.com>
2017-04-15 13:53:22 -0500

    updated book model

--- model/book.js
+++ model/book.js
@@ -1,3 +1,29 @@
 /**
  * Created by vinodh on 4/15/17.
  */
+'use strict';
+
+import mongoose from 'mongoose';
+
+var bookSchema = new mongoose.Schema({
+    title: String,
+    published: {
+        type: Date,
+        default: Date.now()
+    },
+    keywords: Array,
+    is_published: Boolean,
+    author: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+    },
+    //embedded sub-document
+    details: {
+        model_number: Number,
+        is_hardcover: Boolean,
+        reviews: Number,
+        rank: Number
+    }
+});
+
+modules.exports = mongoose.model('Books', bookSchema);
